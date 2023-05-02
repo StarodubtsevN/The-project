e2951a0467b736dfe91f054233f87efb4b2e8f54
NIKOLAI-STARODUBTSEV <starodubtsev_2004@bk.ru>
2023-05-02 11:57:14 +0300

    02 may 2023

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
-    $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
-    $password = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
+    $login = filter_var(trim($_POST['login']),
+        FILTER_SANITIZE_STRING);
+    $password = filter_var(trim($_POST['pass']),
+        FILTER_SANITIZE_STRING);
 
     if(mb_strlen($login) < 5)
     {
      echo "Недопустимая длина логина";
       exit();
     }
+
     else if(mb_strlen($password) < 3 || mb_strlen($password) > 12)
-        {
-            echo "Недопустимая длина пароля(от 2 до 12 символов)";
-        }
-        exit();// если выполнится это условие, то код, который будет ниже не выполнится
+     {
+         echo "Недопустимая длина пароля(от 2 до 12 символов)";
+          exit();// если выполнится это условие, то код, который будет ниже не выполнится
+     }
 
 
-    $mysql = new mysqli("localhost","root","root","register");
+    /*$mysql = new mysqli("localhost","root","root","register");
 
-    $mysql->query('INSERT INTO "users" ("login", "password"), Values ($login, $password)');
-    $mysql->close();
+    $mysql->query("INSERT INTO `users`(login, password), Values ($login, $password)");
+    $mysql->close();*/
 ?>
