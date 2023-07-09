f76644b9ac6043f2445cb9e76bd2feb557db8906
NikolaiStar <starodubtsev_2004@bk.ru>
2023-07-09 15:56:11 +0300

    09.07.23

--- code/account/index.php
+++ code/account/index.php
@@ -5,4 +5,22 @@ $arr = $_POST['API'];
 
     // обработаем полученные данные
 
+if (isset($_GET['formSubmit'])){
+    $_form=$_GET['id'];
+    $form=$_GET['API'];
+    $mysqli = new mysqli("localhost", "root","", "API");
+    if ($mysqli ->connect_errno){
+        echo "Извините возникла проблема :(";
+        exit;
+    }
+    $id = '"'.$mysqli->real_escape_string($_form).'"';
+    $API = '"'.$mysqli->real_escape_string($form).'"';
+    $query = "INSERT INTO API (id,API) VALUES($id,$API)";
+    $result = $mysqli->query($query);
+
+        if($result){
+            print ('Получилось !'. '<br>');
+        }
+    $mysqli->close();
+}
 ?>
