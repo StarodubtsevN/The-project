8605e9065b1f493f875f0f0edb878b5b3a749a2c
NikolaiStar <starodubtsev_2004@bk.ru>
2023-07-17 14:29:15 +0300

    17.07.23

--- code/account/index.php
+++ code/account/index.php
@@ -1,26 +1,21 @@
 <?php
-//получение данных с формы
 
-$arr = $_POST['US_API'];
-
-// обработаем полученные данные
-
-if (isset($_GET['formSubmit'])){
+if (isset($_GET['API'])){
     $_form=$_GET['id'];
     $form=$_GET['api'];
     $mysqli = new mysqli("localhost", "root","", "API");
     if ($mysqli ->connect_errno){
         echo "Извините возникла проблема :(";
-        exit;
+        mysqli_connect_error();
     }
+
     $id = '"'.$mysqli->real_escape_string($_form).'"';
     $API = '"'.$mysqli->real_escape_string($form).'"';
     $query = "INSERT INTO US_API (id,API) VALUES($id,$API)";
     $result = $mysqli->query($query);
 
-    if($result) {
-        echo "Получилось !" . "<br>";
+    if($result){
+        print ('Получилось !'. '<br>');
     }
     $mysqli->close();
 }
-
