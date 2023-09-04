78a598520c7d35b0f8d493ddcdd40f784578986f
NikolaiStar <starodubtsev_2004@bk.ru>
2023-09-04 17:17:05 +0300

    04.09.23

--- code/account/index.php
+++ code/account/index.php
@@ -1,12 +1,13 @@
 <?php
-
 $dbc = mysqli_connect("localhost", "root", "", "API");
 
 if (isset($_POST['api'])){
-
     $API=$_POST['api'];
 
-    if (empty($_POST['api'])) exit("Поле не заполнено");
+    if (empty($_POST['api'])){
+        echo ("Поле не заполнено");
+        exit;
+    }
 
     if ($dbc->connect_errno) {
         echo "Извините возникла проблема :(";
@@ -20,7 +21,6 @@ if (isset($_POST['api'])){
     $result = $dbc->query($query);
 
     if ($result) echo "Получилось !";
-
     mysqli_close($dbc);
 }
 
