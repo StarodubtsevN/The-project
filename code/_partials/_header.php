deb79b3e23de0a2d4fb97afbe5cab65e227d7c25
NikolaiStar <starodubtsev_2004@bk.ru>
2023-09-09 19:59:28 +0300

    09.08.2023

--- code/_partials/_header.php
+++ code/_partials/_header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>"P&T"</title>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/style.css">
 </head>
--- code/auth/_log_sub.php
+++ code/auth/_log_sub.php
@@ -4,9 +4,9 @@
         <h1>Вход</h1>
 
         <form action="_log_sub_old.php" method="post">
-            <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин">
+            <input type="text" class="form-control" name="login" type="email" placeholder="Введите email">
           <br>
-          <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль">
+          <input type="password" class="form-control" name="pass" type="pass" placeholder="Введите пароль">
           <br>
           <button class="btn btn-success" type="submit">Войти</button>
         </form>
