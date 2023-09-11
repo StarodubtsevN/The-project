36a07a71ac13803bbac76cc1cd5684d8c6f4c91a
NikolaiStar <starodubtsev_2004@bk.ru>
2023-09-11 13:05:10 +0300

    11.09.23(5)

--- code/account/API.php
+++ code/account/API.php
@@ -15,31 +15,3 @@
 </body>
 </html>
 
-<?php
-
-$dbc = mysqli_connect("localhost", "root", "", "API");
-
-if (isset($_POST['api'])) {
-    $API = $_POST['api'];
-
-    if (empty($_POST['api'])) {
-        echo("Поле не заполнено");
-        exit;
-    }
-
-    if ($dbc->connect_errno) {
-        echo "Извините возникла проблема :(";
-        exit;
-    }
-
-    $api = '"' . $dbc->real_escape_string($API) . '"';
-
-    $query = "INSERT INTO US_API (api_key) VALUES('$api')";
-
-    $result = $dbc->query($query);
-
-    if ($result) echo "Получилось !";
-    mysqli_close($dbc);
-}
-
-?>
