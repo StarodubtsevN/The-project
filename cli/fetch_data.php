7263e48ea83778b69fc8529770864ba88c48fbc2
NikolaiStar <starodubtsev_2004@bk.ru>
2023-10-08 22:39:15 +0300

    08.10.23(2)

--- cli/fetch_data.php
+++ cli/fetch_data.php
@@ -1,52 +1,57 @@
+<pre>
 <?php
+session_start();
 $dbc= mysqli_connect("localhost", "root", "", "API");
 
 if ($dbc->connect_error) {
     die("Ошибка соединения: " . $dbc->connect_error);
 }
 
-
 $options = array(
-    'updated_on' => '>=2023-10-05T16:35:00Z',
+    'updated_on' => '>=2023-10-04T16:35:00Z',
 );
 
-// @todo поработать над этим
-// $projectSettigns = SELECT * FROM project_settings WHERE user_id = $_SESSION['id']]
-// $options = array(
-//    'updated_on' => '>=' . $projectSettigns['last_update_date'],
-//);
-// $url = ['project_url'] . '/issues.json?' . http_build_query($options);
-// $headers[] = 'X-Redmine-API-Key:' . $projectSettings['api_key'];
+$session_id = $_SESSION['id'];
 
-$url = "https://redmine.shellpea.com/projects/p-n-t/issues.json?" . http_build_query($options);
+$projectSettigns = $dbc->prepare("SELECT * FROM project_settings WHERE user_id = $session_id");
+$projectSettigns->execute();
+$projectSettigns->bind_result($id, $userId, $api_key, $url, $updated_at);
+$projectSettigns->fetch();
+$projectSettigns->close();
+var_dump($userId, $api_key, $url, $updated_at);
 
-$headers = [
-    'X-Redmine-API-Key: b2ccdfa95816fc40268d96246195f6845b7581da',
-    'Content-Type: application/json',
-];
+/*$options = [
+    'updated_on' => '>=' . $updated_at,
+ ];*/
+
+$url_p = $url . '.json?' . http_build_query($options);
+ $headers[] =  'X-Redmine-API-Key: ' . $api_key;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $url_p);
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $response = curl_exec($ch);
-
 $data = json_decode($response, true);
-//var_dump($data);
-
+var_dump($data);
 foreach ($data['issues'] as $issue) {
     $status = $dbc->real_escape_string($issue['status']['name']);
     $issueId = $issue['id'];
 
-    $query = "INSERT INTO tasks (status, task_id) VALUES('$status', $issueId)";
-
+    $query = "INSERT INTO tasks (status, task_id) VALUES('$status', '$issueId')";
     $result = $dbc->query($query);
+
 }
-
 curl_close($ch);
+?>
+</pre>
 
-//curl -H "Content-Type: application/json" -H "X-Redmine-API-Key: b2ccdfa95816fc40268d96246195f6845b7581da" -X GET https://redmine.shellpea.com/projects/p-n-t/issues
+<!-- Comments(old scripts)
+//$url = "https://redmine.shellpea.com/projects/p-n-t/issues.json?" . http_build_query($options);
 
-
+/*$headers = [
+'X-Redmine-API-Key: b2ccdfa95816fc40268d96246195f6845b7581da',
+'Content-Type: application/json',
+];*/-->
